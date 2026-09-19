2d533f741b49f8001a32837ef0556058a56903cd
dreamer6680 <1468683855@qq.com>
2026-09-19 16:00:51 +0800

    security: restrict order read endpoints to sales and manager roles

--- src/main/java/com/packflow/app/order/OrderController.java
+++ src/main/java/com/packflow/app/order/OrderController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
+@PreAuthorize("hasAnyRole('SALES', 'MANAGER')")
 public class OrderController {
     private final OrderService service;
 
